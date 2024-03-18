9c67f69e4a520f9c83af8e24f326fc8b9ad9a01e
Lewis Hardcastle <personal@lewishardcastle.io>
2024-03-18 11:27:16 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,96 @@
+import { faker } from 'https://esm.sh/@faker-js/faker';
+
+// User profile, pic, display name, follow button
+// User caption
+// commenter profile
+// commenter comment, date, of comment, replies, like button
+// post stats, likes, date, like button, comment button, dm button, bookmark button
+// comment form, emoji button, post button
+
+const postImage = grabEl('.post-img');
+const userProfilePic = grabEl('.profile-pic');
+const userName = grabEl('.username');
+const userLocation = grabEl('.user-location');
+
+function grabEl(selector) {
+  return document.querySelector(selector);
+}
+
+function generatePerson() {
+  const firstName = faker.person.firstName();
+  const lastName = faker.person.lastName();
+
+  const displayName = faker.internet.displayName({
+    firstName: `${firstName}`,
+    lastName: `${lastName}`,
+  });
+
+  const userName = faker.internet.userName({
+    firstName: `${firstName}`,
+    lastName: `${lastName}`,
+  });
+
+  return { displayName, userName };
+}
+
+function generateText() {
+  const text = faker.word.words({ count: { min: 1, max: 80 } });
+  return text;
+}
+
+function trueOrFalse() {
+  const value = Math.round(Math.random() * 2);
+  if (value === 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function generatePicture() {
+  const picOrNoPic = trueOrFalse();
+  if (picOrNoPic) {
+    return faker.image.url();
+  } else {
+    return null;
+  }
+}
+
+function likesGenerator() {
+  const likes = Math.round(Math.random() * 1000);
+  return likes;
+}
+
+function getCurrentDate() {
+  const date = new Date();
+  return date;
+}
+
+function generatePost() {
+  const { displayName, userName } = generatePerson();
+
+  const postObj = {
+    userName,
+    location: faker.location.city(),
+    profilePic: faker.image.avatarGitHub(),
+    postImage: faker.image.urlLoremFlickr({
+      width: 800,
+      height: 1000,
+    }),
+    date: getCurrentDate(),
+    likes: likesGenerator(),
+  };
+  return postObj;
+}
+
+function displayUser() {
+  const postObj = generatePost();
+  userName.innerText = postObj.userName;
+  userLocation.innerText = postObj.location;
+  postImage.src = postObj.postImage;
+  userProfilePic.src = postObj.profilePic;
+}
+
+window.addEventListener('load', function () {
+  displayUser();
+});
